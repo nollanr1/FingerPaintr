6108b46c5a29e67c0959416504fa5aa787b059a0
Robert Nollan <nollanr1@spu.edu>
2022-09-10 14:04:13 -0700

    Got the canvas element bootstrapped, and found some resources to get started with. One hour down, 99 to go... Unless I can count the hours of research before I selected this project in particular? (I discarded a handful of other ideas for not being pure HTML/CSS/JavaScript.)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FingerPaintr
 This is liable to take me 100 hours to complete... or such is the intent.
+
+For now, I'm just experimenting with the canvas element to make a simple drawing program in browser.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(10, 10, 100, 100);
 
+
+/*
+findCanvasCoords:
+Finds the mouse coordinates within the canvas (although it could work for any object).
+The following code is stolen from
+https://nerdparadise.com/programming/javascriptmouseposition
+although I've tweaked it a bit.
+*/
+function findCanvasCoords(mouseEvent)
+{
+  let obj = document.getElementById("theCanvas");
+  let obj_left = 0;
+  let obj_top = 0;
+  let xpos;
+  let ypos;
+  while (obj.offsetParent)
+  {
+    obj_left += obj.offsetLeft;
+    obj_top += obj.offsetTop;
+    obj = obj.offsetParent;
+  }
+  if (mouseEvent)
+  {
+    //FireFox
+    xpos = mouseEvent.pageX;
+    ypos = mouseEvent.pageY;
+  }
+  else
+  {
+    //IE
+    xpos = window.event.x + document.body.scrollLeft - 2;
+    ypos = window.event.y + document.body.scrollTop - 2;
+  }
+  xpos -= obj_left;
+  ypos -= obj_top;
+  document.getElementById("objectCoords").innerHTML = xpos + ", " + ypos;
+}
+document.getElementById("theCanvas").onmousemove = findCanvasCoords;
