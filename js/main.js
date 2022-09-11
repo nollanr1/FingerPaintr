c7b8399c8baa3da53d57090ec1ffbb725f10002c
Robert Nollan <nollanr1@spu.edu>
2022-09-11 14:05:37 -0700

    Replaced most of the JS with Mozilla demo code, which looks a lot nicer than what I'd been using. I've got custom color and brush size in place, so I'll tackle export (and maybe import) next. Another hour down, 98 to go...

--- js/main.js
+++ js/main.js
@@ -1,43 +1,59 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'red';
-ctx.fillRect(10, 10, 100, 100);
+//The dreaded global variables.
+//I believe it's more effective to ping the document once when mouse down,
+//Then reference these while drawing.
+//Doing away with these globals means I have to ping the document constantly
+//To get the right color and thickness
+let brushColor = '#000';
+let brushThickness = 3;
 
+//We'll use the demo code from Moz as the foundation,
+//since they apparently did the thing I intend already...
+//It's been bushwhacked a bit and probably will be a lot more as things progress
 
-/*
-findCanvasCoords:
-Finds the mouse coordinates within the canvas (although it could work for any object).
-The following code is stolen from
-https://nerdparadise.com/programming/javascriptmouseposition
-although I've tweaked it a bit.
-*/
-function findCanvasCoords(mouseEvent)
-{
-  let obj = document.getElementById("theCanvas");
-  let obj_left = 0;
-  let obj_top = 0;
-  let xpos;
-  let ypos;
-  while (obj.offsetParent)
-  {
-    obj_left += obj.offsetLeft;
-    obj_top += obj.offsetTop;
-    obj = obj.offsetParent;
+// When true, moving the mouse draws on the canvas
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const theCanvas = document.getElementById('theCanvas');
+const context = theCanvas.getContext('2d');
+
+// event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+theCanvas.addEventListener('mousedown', (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+  brushColor = document.getElementById('color').value; //Modifies the global above
+  brushThickness = document.getElementById('thickness').value; //This also modifies a global
+});
+
+theCanvas.addEventListener('mousemove', (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
   }
-  if (mouseEvent)
-  {
-    //FireFox
-    xpos = mouseEvent.pageX;
-    ypos = mouseEvent.pageY;
+});
+
+window.addEventListener('mouseup', (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
   }
-  else
-  {
-    //IE
-    xpos = window.event.x + document.body.scrollLeft - 2;
-    ypos = window.event.y + document.body.scrollTop - 2;
-  }
-  xpos -= obj_left;
-  ypos -= obj_top;
-  document.getElementById("objectCoords").innerHTML = xpos + ", " + ypos;
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = brushColor; //brushColor and brushThickness are globals
+  context.lineWidth = brushThickness;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
 }
-document.getElementById("theCanvas").onmousemove = findCanvasCoords;
+// End Moz demo code.
+// My thanks to you (and this is sincere).
