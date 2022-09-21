4855e9ad580f40b07ddde1b1be732f896a05a179
Robert Nollan <nollanr1@spu.edu>
2022-09-20 17:17:26 -0700

    Added TODO information about index.ejs, for logged-in users

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,6 @@
 module.exports = {
 	getIndex: (req, res) => {
-	  //res.render("index.ejs");
-	  res.send("index.html"); //TODO: Only send this if user is not logged in. This is the default page.
-	  //
+	  //res.render("index.ejs", {innards: innards, canvas: canvas}); //TODO: Send this if the user is logged in, with personalization (such as name, chosen canvas contents) for them.
+	  res.send("index.html"); //TODO: Only send this if user is not logged in.
 	},
   };  
