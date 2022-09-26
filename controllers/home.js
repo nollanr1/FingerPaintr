38f05ffde3cd55e1c48c2e481ac9971187c61d5b
Robert Nollan <nollanr1@spu.edu>
2022-09-26 14:51:18 -0700

    You can now select an image from the gallery to scribble on, but this taints the canvas. Perhaps this could be fixed by proxying the image through the server I control, but I don't know how to do that yet.

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,30 @@
+const Painting = require("../models/Painting");
+
 module.exports = {
 	getIndex: (req, res) => {
 		console.log('Sending standard homepage');
 		res.render("index.ejs");
 	},
-	getCanvas:(req, res) => {
+	getCanvas: async (req, res) => {
 		console.log(`Sending ejs canvas to ${req.user.userName}`);
-		res.render("canvas.ejs", {userName: req.user.userName, initCanvas: null/*"https://placekitten.com/600/600"/*req.canvasVal*/}); //TODO: Pass the canvas initialization link
+		if(req.params.canvasVal){
+			try {
+				const paintingObject = await Painting.findById(req.params.canvasVal);
+				console.log(`paintingObject: ${paintingObject}`);
+				console.log(`params.canvasVal: ${req.params.canvasVal}`);
+				console.log(`_id: ${paintingObject.id}`);
+				console.log(`image: ${paintingObject.image}`);
+				res.render("canvas.ejs", {userName: req.user.userName, initCanvas: paintingObject.image || null});
+			}
+			catch (err) {
+				console.log(err);
+				console.log('Running a null canvas due to server error')
+				res.render("canvas.ejs", {userName: req.user.userName, initCanvas: null});
+			}
+		}
+		else {
+			console.log('No image provided, so blank canvas.')
+			res.render("canvas.ejs", {userName: req.user.userName, initCanvas: null});
+		}
 	},
   };  
